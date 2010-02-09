fd985064a34e652b89598451ac385d5c4da0dfe1
yappo <yappo@shibuya.pl>
2010-02-09 13:53:37 +0900

    initial commit

---
+++ lib/Module/Setup/Core.pm
@@ -0,0 +1,32 @@
+package Module::Setup::Core;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Module::Setup::Core -
+
+=head1 SYNOPSIS
+
+  use Module::Setup::Core;
+
+=head1 DESCRIPTION
+
+Module::Setup::Core is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
